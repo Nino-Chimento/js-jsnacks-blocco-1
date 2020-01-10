ac152fbc9117f7d081f5e393cc31579ab62eea26
Nino Chimento <saninoc84@gmail.com>
2020-01-10 15:50:42 +0100

    snack 4 con ordinamento lista e assegnazione posto

--- snack4/script.js
+++ snack4/script.js
@@ -8,8 +8,14 @@ for (var i = 0; i < listaInvitati.length; i++) {
   console.log(listaInvitati[i]);
   if (nomeInvitato == listaInvitati[i]) {
     messaggio = "benvenuto"
-    console.log(listaInvitati[i]);
+    utenteInvitato =nomeInvitato;
   }
-
 }
-alert(messaggio)
+
+listaInvitati.sort();
+console.log(listaInvitati);
+ilTuoPosto = listaInvitati.indexOf(utenteInvitato) + 1
+
+
+
+alert(messaggio + "il tuo posto a sedere e' il N"+ ilTuoPosto)
